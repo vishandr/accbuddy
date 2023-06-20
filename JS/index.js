0b218d937a27c9746fd1d26f54581cd271117667
vishandr <vishandr@gmail.com>
2023-06-20 14:32:29 +0300

    1.3

--- JS/index.js
+++ JS/index.js
@@ -264,20 +264,7 @@ function createAccuntsListOutput(accountsList) {
 }
 
 function setCookie(name, value) {
-  // if (options.expires instanceof Date) {
-  //   options.expires = options.expires.toUTCString();
-  // }
-
-  let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
-
-  // for (let optionKey in options) {
-  //   updatedCookie += "; " + optionKey;
-  //   let optionValue = options[optionKey];
-  //   if (optionValue !== true) {
-  //     updatedCookie += "=" + optionValue;
-  //   }
-  // }
-  document.cookie = updatedCookie;
+  document.cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
 }
 
 function buyButton(event){
