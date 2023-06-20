b07c5146c3799ad92e827e22f7593c30f9499c33
vishandr <vishandr@gmail.com>
2023-06-20 14:39:50 +0300

    1.4

--- JS/index.js
+++ JS/index.js
@@ -273,7 +273,7 @@ function buyButton(event){
   console.log(account);
   localStorage.setItem("account", JSON.stringify(account));
   // document.cookie = 'account_id='+id; // workable
-  setCookie('name', 'John')
+  setCookie('id', account.id)
   // setCookie(account.id, account.provider, {'price=': account.price, secure: true, samesite: lax })
   // document.cookie = '_account_id_='+ account.id +';' + 'provider='+ account.provider +';' + 'price=' + account.price
 };
